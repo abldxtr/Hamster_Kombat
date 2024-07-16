b165bcfa9a649bcf4e79f2cd2b52ed8905621906
abldxtr <110703641+abldxtr@users.noreply.github.com>
2024-07-16 01:31:03 +0000

    number font

--- components/counter.tsx
+++ components/counter.tsx
@@ -11,13 +11,15 @@ export default function Counter() {
 
   return (
     <div className="px-4 mt-4 flex justify-center z-10 isolate ">
-      <div className="px-4 py-2 flex items-center space-x-2 ">
+      <div className="px-4 py-2 flex items-center space-x-2 numb">
         <img
           src="https://i.imgur.com/ykQVitW.png"
           alt="dollar coin"
           className=" size-10 aspect-square  "
         />
-        <div className=" text-4xl text-white ">{numberWithCommas(points)}</div>
+        <div className=" text-4xl text-white numb">
+          {numberWithCommas(points)}
+        </div>
       </div>
     </div>
   );
